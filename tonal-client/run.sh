8c9ee40f838c73b530741521d286425b3b513e1a
chubban-lgtm <229042634+chubban-lgtm@users.noreply.github.com>
2026-09-20 13:50:42 -0500

    Use Home Assistant MQTT service credentials

--- tonal-client/run.sh
+++ tonal-client/run.sh
@@ -3,4 +3,11 @@ set -e
 
 echo "[Tonal Client] Starting..."
 
-node /app/service.mjs
+export MQTT_HOST="$(bashio::services mqtt host)"
+export MQTT_PORT="$(bashio::services mqtt port)"
+export MQTT_USERNAME="$(bashio::services mqtt username)"
+export MQTT_PASSWORD="$(bashio::services mqtt password)"
+
+echo "[Tonal Client] MQTT service credentials loaded."
+
+exec node /app/service.mjs
